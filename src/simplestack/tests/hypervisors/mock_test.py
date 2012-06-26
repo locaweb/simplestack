338b1c9011e409bf86174326b3803f17ef5e4416
PotHix <pothix@pothix.com>
2012-06-26 18:09:08 -0300

    Adding license headers.

--- src/simplestack/tests/hypervisors/mock_test.py
+++ src/simplestack/tests/hypervisors/mock_test.py
@@ -1,8 +1,27 @@
-import unittest
-import random
+# Copyright 2012 Locaweb.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+# @author: Thiago Morello (morellon), Locaweb.
+# @author: Willian Molinari (PotHix), Locaweb.
+
 from simplestack.exceptions import FeatureNotImplemented
 from simplestack.hypervisors import mock
 from simplestack.tests.hypervisors.base_test_case import HypervisorBaseTest
+
+import random
+import unittest
 
 
 class MockTest(unittest.TestCase, HypervisorBaseTest):
--- src/simplestack/utils/vmware.py
+++ src/simplestack/utils/vmware.py
@@ -1,11 +1,32 @@
-import re
-import uuid
+# Copyright 2012 Locaweb.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+# @author: Francisco Freire, Locaweb.
+# @author: Thiago Morello (morellon), Locaweb.
+# @author: Willian Molinari (PotHix), Locaweb.
+
 from pysphere import VIServer, VITask
 from pysphere.vi_property import VIProperty
 from pysphere.vi_virtual_machine import VIVirtualMachine
 from pysphere.resources import VimService_services as VI
 from pysphere.resources import VimService_services_types as VITypes  # ns0.VirtualCdromIsoBackingInfo_Def
 from simplestack.exceptions import HypervisorError
+
+import re
+import uuid
+
 VirtualCdromIsoBackingInfo = VITypes.ns0.VirtualCdromIsoBackingInfo_Def(None).pyclass
 
 
