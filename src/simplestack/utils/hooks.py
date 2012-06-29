3b792d5c367931472963289a179da594863e5c6e
Thiago Morello <thiago.morello@locaweb.com.br>
2012-06-29 15:10:36 -0300

    Scratching hooks

--- src/simplestack/utils/hooks.py
+++ src/simplestack/utils/hooks.py
@@ -12,6 +12,7 @@ def not_implemented(f):
 class not_implemented_all(object):
     def __init__(self, subject):
         self.subject = subject
+        self.regex = r'^[^_]'
         self.__redefine_clazz__()
 
     def __call__(self, *args, **kws):
@@ -21,17 +22,21 @@ class not_implemented_all(object):
         instancemethod = type(self.__redefine_clazz__)
         for old_method_name in dir(self.subject):
             old_method = getattr(self.subject, old_method_name)
-            if type(old_method) is instancemethod and re.match(r'^[^_]', old_method_name):
+            if type(old_method) is instancemethod and re.match(self.regex, old_method_name):
                 setattr(self.subject, old_method_name, not_implemented(old_method))
 
-# @not_implemented_all
+@not_implemented_all
 class Abc(object):
     def __init__(self):
         pass
 
-    @not_implemented
     def hello(self):
         print "OLA"
 
     def ola(self):
         print "XUPA"
+
+a = Abc()
+
+a.hello()
+a.ola()
