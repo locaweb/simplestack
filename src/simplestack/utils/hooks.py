2bbc9412d8308812040ce2c2907effb6d00a165e
Thiago Morello <morellon@gmail.com>
2012-06-29 18:28:10 -0300

    Playing with hooks

--- src/simplestack/utils/hooks.py
+++ src/simplestack/utils/hooks.py
@@ -1,15 +1,6 @@
 import re
 
-def not_implemented(f):
-    def wrapped(*args, **kws):
-        try:
-            print("NEW METHOD")
-            return f(*args, **kws)
-        except:
-            print("EXCEPT")
-    return wrapped
-
-class not_implemented_all(object):
+class hook_all(object):
     def __init__(self, subject):
         self.subject = subject
         self.regex = r'^[^_]'
@@ -23,12 +14,26 @@ class not_implemented_all(object):
         for old_method_name in dir(self.subject):
             old_method = getattr(self.subject, old_method_name)
             if type(old_method) is instancemethod and re.match(self.regex, old_method_name):
-                setattr(self.subject, old_method_name, not_implemented(old_method))
+                setattr(self.subject, old_method_name, self.hook(old_method))
+
+def not_implemented(f):
+    def wrapped(*args, **kws):
+        # ADD CODE HERE
+        f_self = args[0]
+        print(f_self.attr)
+        print("NEW METHOD")
+        return f(*args, **kws)
+    return wrapped
+
+class not_implemented_all(hook_all):
+    def __init__(self, subject):
+        self.hook = not_implemented
+        super(self.__class__, self).__init__(subject)
 
 @not_implemented_all
 class Abc(object):
     def __init__(self):
-        pass
+        self.attr = "an attribute"
 
     def hello(self):
         print "HELLO"
@@ -37,6 +42,5 @@ class Abc(object):
         print "OLA"
 
 a = Abc()
-
 a.hello()
 a.ola()
