626365bb6d654dfc4a58010bc160f3fcf6177f98
ncode <juliano.martinez@locaweb.com.br>
2012-07-04 18:02:10 +0000

    added -u simplestack to start-stop-daemon

--- debian/init.d
+++ debian/init.d
@@ -43,7 +43,7 @@ do_start()
     #   0 if daemon has been started
     #   1 if daemon was already running
     #   other if daemon could not be started or a failure occured
-    start-stop-daemon -b --start --quiet --exec $DAEMON -- $DAEMON_ARGS
+    start-stop-daemon -u simplestack -b --start --quiet --exec $DAEMON -- $DAEMON_ARGS
 }
 
 do_stop()
@@ -55,79 +55,77 @@ do_stop()
     #   0 if daemon has been stopped
     #   1 if daemon was already stopped
     #   other if daemon could not be stopped or a failure occurred
-    start-stop-daemon --stop --quiet --signal KILL --retry=TERM/30/KILL/5 --pidfile $PIDFILE && rm $PIDFILE
+    start-stop-daemon -u simplestack --stop --quiet --signal KILL --retry=TERM/30/KILL/5 --pidfile $PIDFILE && rm $PIDFILE
 }
 
 #
 # Tell rsyslogd to reload its configuration
 #
 do_reload() {
-   NAME="$LOCASTACK"
-   PIDFILE="$LOCASTACK_PIDFILE"
-   start-stop-daemon --stop --signal HUP --quiet --pidfile $PIDFILE --name $NAME
+    NAME="$LOCASTACK"
+    PIDFILE="$LOCASTACK_PIDFILE"
+    start-stop-daemon -u simplestack --stop --signal HUP --quiet --pidfile $PIDFILE --name $NAME
 }
 
 create_xconsole() {
-   XCONSOLE=/dev/xconsole
-   if [ "$(uname -s)" = "GNU/kFreeBSD" ]; then
-       XCONSOLE=/var/run/xconsole
-       ln -sf $XCONSOLE /dev/xconsole
-   fi
-   if [ ! -e $XCONSOLE ]; then
-       mknod -m 640 $XCONSOLE p
-       chown root:adm $XCONSOLE
-       [ -x /sbin/restorecon ] && /sbin/restorecon $XCONSOLE
-   fi
+    XCONSOLE=/dev/xconsole
+    if [ "$(uname -s)" = "GNU/kFreeBSD" ]; then
+        XCONSOLE=/var/run/xconsole
+        ln -sf $XCONSOLE /dev/xconsole
+    fi
+    if [ ! -e $XCONSOLE ]; then
+        mknod -m 640 $XCONSOLE p
+        chown root:adm $XCONSOLE
+        [ -x /sbin/restorecon ] && /sbin/restorecon $XCONSOLE
+    fi
 }
 
 sendsigs_omit() {
-   OMITDIR=/lib/init/rw/sendsigs.omit.d
-   mkdir -p $OMITDIR
-   rm -f $OMITDIR/rsyslog
-   ln -s $LOCASTACK_PIDFILE $OMITDIR/rsyslog
+    OMITDIR=/lib/init/rw/sendsigs.omit.d
+    mkdir -p $OMITDIR
+    rm -f $OMITDIR/rsyslog
+    ln -s $LOCASTACK_PIDFILE $OMITDIR/rsyslog
 }
 
 case "$1" in
- start)
-   log_daemon_msg "Starting $DESC" "$LOCASTACK"
-   create_xconsole
-   do_start
-   case "$?" in
-       0) sendsigs_omit
-          log_end_msg 0 ;;
-       1) log_progress_msg "already started"
-          log_end_msg 0 ;;
-       *) log_end_msg 1 ;;
-   esac
-
-   ;;
- stop)
-   log_daemon_msg "Stopping $DESC" "$LOCASTACK"
-   do_stop
-   case "$?" in
-       0) log_end_msg 0 ;;
-       1) log_progress_msg "already stopped"
-          log_end_msg 0 ;;
-       *) log_end_msg 1 ;;
-   esac
-
-   ;;
- reload|force-reload)
-   log_daemon_msg "Reloading $DESC" "$LOCASTACK"
-   do_reload
-   log_end_msg $?
-   ;;
- restart)
-   $0 stop
-   $0 start
-   ;;
- status)
-   status_of_proc -p $LOCASTACK_PIDFILE $LOCASTACK_BIN $LOCASTACK && exit 0 || exit $?
-   ;;
- *)
-   echo "Usage: $SCRIPTNAME {start|stop|restart|reload|force-reload|status}" >&2
-   exit 3
-   ;;
+    start)
+        log_daemon_msg "Starting $DESC" "$LOCASTACK"
+        create_xconsole
+        do_start
+        case "$?" in
+            0) sendsigs_omit
+               log_end_msg 0 ;;
+            1) log_progress_msg "already started"
+               log_end_msg 0 ;;
+            *) log_end_msg 1 ;;
+        esac
+    ;;
+    stop)
+        log_daemon_msg "Stopping $DESC" "$LOCASTACK"
+        do_stop
+        case "$?" in
+            0) log_end_msg 0 ;;
+            1) log_progress_msg "already stopped"
+               log_end_msg 0 ;;
+            *) log_end_msg 1 ;;
+        esac
+    ;;
+    reload|force-reload)
+        log_daemon_msg "Reloading $DESC" "$LOCASTACK"
+        do_reload
+        log_end_msg $?
+    ;;
+    restart)
+        $0 stop
+        $0 start
+    ;;
+    status)
+        status_of_proc -p $LOCASTACK_PIDFILE $LOCASTACK_BIN $LOCASTACK && exit 0 || exit $?
+    ;;
+    *)
+        echo "Usage: $SCRIPTNAME {start|stop|restart|reload|force-reload|status}" >&2
+        exit 3
+    ;;
 esac
 
 :
